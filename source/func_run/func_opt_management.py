508ac66ea45fcacee34908e585e1541c524d5707
joaK <kruithof@pequan.lip6.fr>
2015-07-09 16:20:27 +0200

    first almost-empty functioning version

--- source/func_run/func_opt_management.py
+++ source/func_run/func_opt_management.py
@@ -1,17 +1,16 @@
 # This file regroups functions for option management
 
-def load_global_options(source, path):
+def load_global_opt(source, path):
     "This function loads fipogen global options and parameters from different data sources, depending on user choice"
     " - file loads params from file "
     " - CLI loads args from CLI"
     " - webapp loads args from webpage"
      
     # Check args consistence
-    if ( is not(( source == "file"   ) or
-                ( source == "webapp" ) or
-                ( source == "CLI"    )):
-        print("load_global_options : arg error"));
-    
+    if ( not( source == "file" or  source == "webapp" or  source == "CLI" )):
+        print("load_global_options : arg error")
+        exit()
 # Dirty hardcoded definitions for global opts until other parts are complete
 
-return
+def set_global_default():
+    "This functions sets default global parameters for the FiPoGen current run"
--- source/run_me.py
+++ source/run_me.py
@@ -4,11 +4,6 @@
 # - standalone app with file-contained args
 # - webapp run with web-input parameters
 
-# Run as Standalone
-
-# Parse arguments and catch errors
-# https://docs.python.org/2/library/argparse.html
-
 import sys
 
 # Adding source folders to PYTHONPATH
@@ -17,26 +12,38 @@ sys.path.append("./func_math");
 sys.path.append("./func_run");
 sys.path.append("./meth");
 
-print(sys.path());
+#print(sys.path);
+
+from func_opt_management import load_global_opt, set_global_default ;
 
 # TEMP
 # Manual override
-is_run_as_cli = False;
-is_run_as_webapp = False;
-is_run_as_standalone = True;
+is_run_as_cli = False ;
+is_run_as_webapp = False ;
+is_run_as_standalone = True ;
 
-# Parse command line args
+# Default value for conf file
+path_file = "./conf_fipogen.py" ;
+
+set_global_default();
+
+# Parse arguments and catch errors
+# https://docs.python.org/2/library/argparse.html
 
 if (__name__ == "__main__"):
     print("FiPoGen running in standalone mode (parameters as file)")
-    load_global_options("file",path_file);
+    load_global_opt("file",path_file) ;
 elif is_run_as_cli:
-    print("FiPoGen running in CLI mode (parameters as CL")
-    load_global_options("CLI");
+    print("FiPoGen running in CLI mode (CL parameters")
+    # Verify and parse args
+    load_global_opt("CLI");
 elif is_run_as_webapp:
     # Print nothing, debug
-    print("FiPoGen runnin in webapp mode, waiting for parameters input in web UI")
-    load_global_options("webapp");
+    print("FiPoGen running in webapp mode, waiting for parameters input in web UI")
+    # Wait for keyboard-chair interface
+    # func_load_webserver ;
+    # func_setup_webserver ;
+    load_global_opt("webapp") ;
     
 
     
