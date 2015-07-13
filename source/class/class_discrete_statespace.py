fe2e528e254b3fbe7087694f22df842371e8963f
joaK <kruithof@pequan.lip6.fr>
2015-07-13 16:21:08 +0200

    class embryo from Thibault's source Amended __repr__ of dSS object Copied random_dss from previous source ...

--- source/class/class_discrete_statespace.py
+++ source/class/class_discrete_statespace.py
@@ -1,5 +1,250 @@
+# coding=utf8
+
 # This class describes the "discrete_ss" class which represents a discrete state Space
 # Original code Thibault Hilaire
 # Refactoring Joachim Kruithof
 # 2015 LIP6
 
+# implementing as pythonic as possible
+# visual representation
+#  help
+# tests
+
+# import numpy as np
+# import scipy as sp
+# Aliasing e.g. using "np." does not work afterwards. Global import is not necessary
+
+from numpy                import matrix as mat
+# Math functions
+from math                 import sqrt
+from numpy                import dot, eye, pi, cos, sin
+from numpy.random         import rand, randn
+# Linear Algebra
+from numpy.linalg         import inv, det, solve
+from numpy.linalg.linalg  import LinAlgError
+
+# import random # standard python random lib ?
+# import slycot # lin eq solver
+
+# Module description
+__author__ = "Thibault Hilaire, FiPoGen Team"
+__email__ = "fipogen@lip6.fr"
+__license__ = "CECILL-C"
+__version__ = "0.0.1"  # Modify this to increment with git scripting
+
+class dSS(object):
+  """The dSS class describes a discrete state space realization
+     ---
+     A state space system is defined as :math:A\in\mathbb{R}^{n \times n}, B\in\mathbb{R}^{n \times p}, C\in\mathbb{R}^{q \times n} and C\in\mathbb{q \times p}, and
+  . . math::
+
+  \left\lbrace\begin{array}{rcl}
+  X(k+1) &=& AX(k) + BU(k) \\
+  Y(k) &=& CX(k) + DU(k)
+
+  n,p,q are the dimensions of the state-space (number of states, inputs and outputs, respectively)
+  """
+  
+  def __init__(self, A, B, C, D):
+    """
+    Construction of a discrete state space
+    """
+    self._A = mat(A)  # User input
+    self._B = mat(B)
+    self._C = mat(C)
+    self._D = mat(D)
+    
+    self._n, self._p, self._q = self.__check_dimensions__()  # Verify coherence and return dimensions
+
+    # Initialize other parts of class (can be computed at class creation)
+    
+    self._Wo, self._Wc = None
+    self._norm_H1, self._norm_H2, self._norm_Hinf = None
+
+    def __check_dimensions__(self):
+      """
+      Computes the number of inputs and outputs.
+      It also checks the concordance of the matrices' size
+      """
+
+      # A
+      a1, a2 = self.A.shape
+    
+      if a1 != a2:
+        raise ValueError, 'A is not a square matrix'
+      n = a1
+
+      # B
+      b1, b2 = self.B.shape
+    
+      if b1 != n:
+        raise ValueError, 'A and B should have the same number of rows'
+      inputs = b2
+
+      # C
+      c1, c2 = self.C.shape
+    
+      if c2 != n:
+        raise ValueError, 'A and C should have the same number of columns'
+      outputs = c1
+
+      # D
+      d1, d2 = self.D.shape
+    
+      if (d1 != outputs or d2 != inputs):
+        raise ValueError, 'D should be consistant with C and B'
+
+      return n, inputs, outputs
+
+      
+    def __str__(self):
+      """
+      Display the state-space
+      """
+        
+      str_mat = "State Space\nA=" + repr(self._A) + "\nB=" + repr(self._B) + "\nC=" + repr(self._C) + "\nD=" + repr(self._D) + "\n"
+
+      if (self._Wc != None or self._Wo != None): 
+        str_obs = "Wc = " + repr(self._Wc) + "\nWo = " + repr(self._Wo) + "\n"
+      else:
+        str_obs = "Wc and Wo not defined\n"
+      
+      if (self._norm_H1 != None or self._norm_H2 != None or self._norm_Hinf != None):
+        str_norm = "norm_H1 = " + repr(self._norm_H1) + "\nnorm_H2 = " + repr(self._norm_H2) + "\nnorm_Hinf = " + repr(self._norm_Hinf)
+      else:
+        str_norm = "norm_H1,norm_H2 and norm_Hinf not defined"
+      
+      str_repr_dss = str_mat + str_obs + str_norm
+    
+      return str_repr_dss
+
+
+    def __repr__(self):
+      return str(self)
+  
+
+  # def _latex_(self):
+    # #TODO
+    # return ""
+    
+# Functions which calculates values from user input    
+    
+    def Wo():
+      return Wo
+
+    def Wc():
+      return Wc   
+    
+# Random state-space
+def random_dSS(n=None, p=1, q=1):
+    """Generate a n-th order random stable state-space, with p inputs and q outputs
+    
+    copy/Adapted from control-python library
+    """
+    
+    if n == None:
+        n = random.randint(5, 10)
+
+    # Probability of repeating a previous root.
+    pRepeat = 0.05
+    # Probability of choosing a real root.  Note that when choosing a complex
+    # root, the conjugate gets chosen as well.  So the expected proportion of
+    # real roots is pReal / (pReal + 2 * (1 - pReal)).
+    pReal = 0.6
+    # Probability that an element in B or C will not be masked out.
+    pBCmask = 0.8
+    # Probability that an element in D will not be masked out.
+    pDmask = 0.3
+    # Probability that D = 0.
+    pDzero = 0.2
+                
+    # Check for valid input arguments.
+    if n < 1 or n % 1:
+            raise ValueError(("states must be a positive integer.  #states = %g." % n))
+    if p < 1 or p % 1:
+        raise ValueError(("inputs must be a positive integer.  #inputs = %g." % p))
+    if q < 1 or q % 1:
+        raise ValueError(("outputs must be a positive integer.  #outputs = %g." % q))
+                
+    # Make some poles for A.  Preallocate a complex array.
+    poles = np.zeros(n) + np.zeros(n) * 0.j
+    i = 0
+                
+    while i < n:
+        if rand() < pRepeat and i != 0 and i != n - 1:
+            # Small chance of copying poles, if we're not at the first or last
+            # element.
+            if poles[i - 1].imag == 0:
+                # Copy previous real pole.
+                poles[i] = poles[i - 1]
+                i += 1
+            else:
+                # Copy previous complex conjugate pair of poles.
+                poles[i:i + 2] = poles[i - 2:i]
+                i += 2
+        elif rand() < pReal or i == n - 1:
+            # No-oscillation pole.
+            poles[i] = 2. * rand() - 1.
+            i += 1
+        else:
+            # Complex conjugate pair of oscillating poles.
+            mag = rand()
+            phase = 2. * pi * rand()
+            poles[i] = complex(mag * cos(phase), mag * sin(phase))
+            poles[i + 1] = complex(poles[i].real, -poles[i].imag)
+            i += 2
+
+    # Now put the poles in A as real blocks on the diagonal.
+    A = np.zeros((n, n))
+    i = 0
+    while i < n:
+        if poles[i].imag == 0:
+            A[i, i] = poles[i].real
+            i += 1
+        else:
+            A[i, i] = A[i + 1, i + 1] = poles[i].real
+            A[i, i + 1] = poles[i].imag
+            A[i + 1, i] = -poles[i].imag
+            i += 2
+    # Finally, apply a transformation so that A is not block-diagonal.
+    while True:
+        T = randn(n, n)
+        try:
+            A = dot(solve(T, A), T)  # A = T \ A * T
+            break
+        except LinAlgError:
+            # In the unlikely event that T is rank-deficient, iterate again.
+            pass
+
+    # Make the remaining matrices.
+    B = randn(n, p)
+    C = randn(q, n)
+    D = randn(q, p)
+
+    # Make masks to zero out some of the elements.
+    while True:
+        Bmask = rand(n, p) < pBCmask 
+        if not Bmask.all():  # Retry if we get all zeros.
+            break
+    
+    while True:
+        Cmask = rand(q, n) < pBCmask
+        if not Cmask.all():  # Retry if we get all zeros.
+            break
+    
+    if rand() < pDzero:
+        Dmask = np.zeros((q, p))
+    else:
+        while True:
+            Dmask = rand(q, p) < pDmask
+            if not Dmask.all():  # Retry if we get all zeros.
+                break
+    
+
+    # Apply masks.
+    B = B * Bmask
+    C = C * Cmask
+    D = D * Dmask
+
+    return dSS(A, B, C, D)
+  
