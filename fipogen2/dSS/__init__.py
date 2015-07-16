6765b256a049a7e18ea7d49c8c6332bcf7c6fe97
joaK <kruithof@pequan.lip6.fr>
2015-07-16 16:56:16 +0200

    add init file

---
+++ fipogen2/dSS/__init__.py
@@ -0,0 +1,8 @@
+#coding=utf8
+
+# Package dSS - part of FiPogen2
+# Discrete state space representation and tools
+
+# List of files for from dSS import *
+
+__all__ = ["class_dSS", "decorators", "random_dSS"]
