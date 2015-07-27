3190a3f650962119512fb28b6c10c48f6c957459
joaK <kruithof@pequan.lip6.fr>
2015-07-27 17:56:27 +0200

    wcpg wrapper (not working, may burn eyes)

--- fipogen2/LTI/WCPG_wrapper/compile_me.sh
+++ fipogen2/LTI/WCPG_wrapper/compile_me.sh
@@ -1,1 +1,4 @@
- 
+#!/bin/bash
+
+# Compile python wrapper => try using distutils first
+#gcc
--- fipogen2/LTI/WCPG_wrapper/setup.py
+++ fipogen2/LTI/WCPG_wrapper/setup.py
@@ -1,1 +1,10 @@
- 
+#coding=utf8
+#package using distutils
+from distutils.core import setup, extension
+import numpy.distutils.misc_util
+
+setup(
+    ext_modules=[Extension("_pyWCPG",["_pyWCPG.c", "chi2.c"])],
+    include_dirs = numpy.distutils.misc_util.get_numpy_include_dirs(),
+  
+)
