043caa947530d48d20e6305cc7d290f3bc069fa5
joaK <kruithof@pequan.lip6.fr>
2015-07-28 17:40:55 +0200

    modify setup.py to include custom opts for linking

--- fipogen2/LTI/WCPG_wrapper/setup.py
+++ fipogen2/LTI/WCPG_wrapper/setup.py
@@ -1,10 +1,19 @@
 #coding=utf8
 #package using distutils
-from distutils.core import setup, extension
+from distutils.core import setup, Extension
 import numpy.distutils.misc_util
 
+#module1 = Extension('_pyWCPG', sources = ['_pyWCPG.c'])
+
+#setup (name='myWCPG',
+       #version = '0.0.9',
+       #description = 'WCPG calculation package',
+       #ext-modules = [module1])
+
 setup(
-    ext_modules=[Extension("_pyWCPG",["_pyWCPG.c", "chi2.c"])],
-    include_dirs = numpy.distutils.misc_util.get_numpy_include_dirs(),
-  
-)
+    ext_modules=[ Extension("_pyWCPG", 
+	          sources=["_pyWCPG.c"], 
+	          extra_compile_args=['-l','libwcpg.so.0.0.9','-rdynamic',''])],
+    
+    include_dirs = numpy.distutils.misc_util.get_numpy_include_dirs())
+
