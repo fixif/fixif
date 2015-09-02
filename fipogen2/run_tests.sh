082e0023969dd28e89a5e6e4b109adf44b56cd9c
joaK <kruithof@pequan.lip6.fr>
2015-09-02 18:31:51 +0200

    minor edits

--- fipogen2/run_tests.sh
+++ fipogen2/run_tests.sh
@@ -2,14 +2,17 @@
 
 # Script used to launch tests for FiPoGen
 
+  clear ;
+  
 # Run all tests
 if [ "$1" = "all" ] ;
 then
-  clear ;
   python -m unittest discover ;
 fi
+
 # Run unit tests, but not all
 #python -m unittest LTI.test.test_dSS
+
 if [ "$1" = "FIPObject" ] ;
 then
   python -m unittest FIPObject.test.test_FIPObject ;
