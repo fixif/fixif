2de35f72bca1bb90a8bd87ff13babfcd7092aecc
joaK <kruithof@pequan.lip6.fr>
2015-09-03 14:51:12 +0200

    lots of modification, implement stubs, modify __del__ method, implement callback for dSS

--- fipogen2/TRK/FIPObject.py
+++ fipogen2/TRK/FIPObject.py
@@ -45,18 +45,25 @@ class FIPObject(object):
     
     idx_data = [[]] ;  # global index
     log_data = [] ;  #  global log
+    log_n_item = 0 ; # number of log entries
 
     idx_n_subclass = 0 ;  # number of FipObject subclasses indexed
     
     idx_subclass = {} ;  # dictionary of FipObject subclasses
 
-    idx_data_fields = {'obj_id':0, 'obj_name':2, 'obj_':3, 'free_id':4} ;
+    idx_data_fields = {'obj_id':0, 'obj_name':1, 'obj_':2, 'free_id':3} ;
     idx_data_type = ['list', 'list', 'list', 'int'] ;
     
     trk_data_fields = {'subclassname':0, 'obj_id':1, 'obj_name':2, 'father':3, 'offsprings':4} ;
     trk_data_type = ['str', 'int', 'str', 'obj', 'list'] ;
     
     log_data_fields = {'e_type':0, 'e_desc':1, 'e_src':2, 'e_opt':3} ;  # 'create' from 'e_desc/import' of object 'e_o_src'
+    
+    event_type=('create', 'destruct') ;
+    
+    #e_src is the caller func.__name__
+    #e_opt is a descriptive field
+    
     log_data_type = ['str', 'str', 'str', 'str'] ;
     
     def _init_idx_subclass_data(self):
@@ -67,28 +74,9 @@ class FIPObject(object):
         need to use idx_subclass[tgt_classname]
         """
         
-        self.idx_data[self.idx_n_subclass] = [[] for k in range(0, len(self.idx_data_fields.values()) - 1)] ;
-
-        # Init self.idx_data, based on self.idx_data_fields & self.idx_data_type
+        FIPObject.idx_data[self.idx_n_subclass] = [[] for k in range(0, len(self.idx_data_fields.values()))] ;
         
-        for i in range(0, len(self.idx_data_fields.values()) - 1):
-            
-            if self.idx_data_type[i] == 'str':
-                selnumberf.idx_data[self.idx_n_subclass][i].append('') ;
-                
-            elif self.idx_data_type[i] == 'int':
-                self.idx_data[self.idx_n_subclass][i].append(-1) ;  # assigned tracknum : >=0
-                
-            elif self.idx_data_type[i] == 'obj': 
-                self.idx_data[self.idx_n_subclass][i].append(None) ;
-                
-            elif self.idx_data_type[i] == 'list': 
-                self.idx_data[self.idx_n_subclass][i].append([]) ;
-                
-            else:
-                raise('FIPObject : unknown self.idx_data_type') ;
-
-            self.idx_data[self.idx_n_subclass][self.idx_data_fields['free_id']] = 0 ;
+        FIPObject.idx_data[self.idx_n_subclass][self.idx_data_fields['free_id']] = 0 ;
         
     def _init_trk_obj_data(self):
         
@@ -96,19 +84,22 @@ class FIPObject(object):
         Initialize the tracking label at instance level (trk_data)
         """
         
-        for i in range(0, len(self.trk_data_fields.values()) - 1):
+        # Create label
+        self.trk_data = [] ;
+        
+        for i in range(0, len(self.trk_data_fields.values())):
         
             if self.trk_data_type[i] == 'str':
-                self.trk_data[i] = '' ;
+                self.trk_data.append('') ;
                 
             elif self.trk_data_type[i] == 'int':
-                self.trk_data[i] = -1 ;  # normal tracknum should always be >=0 ;
+                self.trk_data.append(-1) ;  # normal tracknum should always be >=0 ;
                 
             elif self.trk_data_type[i] == 'obj':
-                self.trk_data[i] = None ;
+                self.trk_data.append(None) ;
                 
             elif self.trk_data_type[i] == 'list':
-                self.trk_data[i] = [];
+                self.trk_data.append([]);
                 
             else:
                 raise('FIPObject : unknown self.trk_data_type') ;
@@ -146,24 +137,19 @@ class FIPObject(object):
         """
         Fill the global index data relative to current instance at FipObject class level
         """
-        print 'AAAAAAAAAAAAAAAAAAAAAAAAAAAA' ;
-        print tgt_subclassname + '\n' ;
-        print repr(self.idx_subclass) + '\n' ;
-        print '\n' ;
-        print type(self.idx_subclass[tgt_subclassname]) ;
-        print repr(self.idx_subclass[tgt_subclassname]) ;
+
+        FIPObject.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['obj_id']].append(self.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['free_id']]) ;
+
+        FIPObject.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['free_id']] += 1 ;
         
-        self.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['obj_id']] = self.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['free_id']] ;
-        self.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['free_id']] += 1 ;
-        
-        self.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['obj_name']] = tgt_subclassname + '_' + str(self.idx_data[tgt_subclassname][self.idx_data_fields['obj_id']]) ;
-        # Pointer to obj, obj is mutable, so no duplication of obj
-        self.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['obj_']] = self ;
+        FIPObject.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['obj_name']].append(tgt_subclassname + '_' + str(self.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['obj_id']])) ;
+        # Pointer to mutable obj, (obj is mutable, so no duplication of obj)
+        FIPObject.idx_data[self.idx_subclass[tgt_subclassname]][self.idx_data_fields['obj_']].append(self) ;
     
     def __init__(self, tgt_subclassname):
 
         """
-        Create index for subclass obj instances at FipObject class level
+        Create index for subclass obj instances at FIPObject class level
         Init tracking label at instance level
         TODO : write event in logger upon call
         """
@@ -173,19 +159,18 @@ class FIPObject(object):
 
         #tgt_classname = self.__class__.__name__ ;  #  verify that name subspace is correctly defined so that no name conflict is possible ?
         
-        if self.is_trk_on:
+        if FIPObject.is_trk_on:
         
-            if not(self.idx_subclass.has_key(tgt_subclassname)):  # CREATE new index for first class encounter
+            if not(FIPObject.idx_subclass.has_key(tgt_subclassname)):  # CREATE new index for first class encounter
                 
-                self.idx_subclass[tgt_subclassname] = self.idx_n_subclass ;
+                FIPObject.idx_subclass[tgt_subclassname] = self.idx_n_subclass ;
                     
-                self._init_idx_subclass_data(self) ;
+                FIPObject._init_idx_subclass_data() ;
                     
-                self.idx_n_subclass += 1 ;
-                
+                FIPObject.idx_n_subclass += 1 ;
                 
             # Register object in global index
-            self._set_idx_obj_data(tgt_subclassname) ;
+            FIPObject._set_idx_obj_data(tgt_subclassname) ;
             
             # Label object
             self._init_trk_obj_data() ;
@@ -194,39 +179,23 @@ class FIPObject(object):
             self._set_trk_obj_data(tgt_subclassname) ;
 
         # idx_subclass[target_obj.__name__][self.trk_data_fields['offsprings']] only iterated on "create from" events
-            
-    def _set_track_data_idx(self, target_obj, father):    
-            
-        self.idx_data[idx_subclass[target_obj.__name__]][self.idx_data_fields['subclassname']].append(target_obj.__name__) ;
-        self.idx_data[idx_subclass[target_obj.__name__]][self.idx_data_fields['obj_id']].append(self.idx_data[idx_subclass[target_obj.__name__]][self.idx_data_fields['free_id']]) ;
-        self.idx_data[idx_subclass[target_obj.__name__]][self.idx_data_fields['obj_name']].append(target_obj.__name__ + '_' + self.idx_data[idx_subclass[target_obj.__name__]][self.idx_data_fields['obj_id']]) ;
-        self.idx_data[idx_subclass[target_obj.__name__]][self.idx_data_fields['obj_']].append(target_obj) ;
-        
-        self.idx_data[idx_subclass[target_obj.__name__]][self.idx_data_fields['free_id']] += 1 ;
-            
 
-            
-
-#     def _get_free_id(self, target_obj, father):
-#         
-#         idx_subclass[target_obj.__name__][self.trk_data_fields['obj_id']] = self.idx_data[idx_subclass[target_obj.__name__]][self.idx_data_fields['free_id']]
-#         
-#         self.idx_data[idx_subclass[target_obj.__name__]][self.idx_data_fields['free_id']] += 1 ;
     
     def _init_local_log(self, target_obj, father):
+    	
         """
-        Local log inumbers defined as a list of lists that should be read from left to right
-        """
-        
-    #def _init_glob_log(self, target_obj, father):    
+        Local log numbers defined as a list of lists that should be read from left to right
+        """  
         
     def send_event(self, **kwargs):  # should be e_type = 'create',''
+    	
         """
         This function must be called whenever an event happens.
         Called by init send_event('create',)
         Log data is not sorting things by class type because it is not an object tracker
         It should reflect what the user / the computer did do
         """
+        
         for key, val in kwargs.items():
             if log_data_fields.has(key):
                 # Fill global event log
@@ -238,13 +207,15 @@ class FIPObject(object):
         
         # Fill history of father
         
-    def get_history(self):    
+    @staticmethod    
+    def get_log(self):    
         # Fill local history
 
         pass ;
         
         # Classe connue de nos services ?
-        
+    
+    @staticmethod    
     def _repr_index(self):
     	
     	"""
@@ -280,3 +251,24 @@ class FIPObject(object):
     	"""
     	
     	pass ;
+    
+    def _del_idx_obj_data(self):
+    	"""
+    	Delete 
+    	"""
+    	# we don't try to get the freed id for another tracked object
+    	
+    
+    def __del__(self):
+    	
+    	"""
+    	The obj destructor must be modified if we want to remove tracking of object when it doesn't exist any more
+    	We remove the entry of the destructerd object in the index	
+    	"""
+    	#register destruction in event log
+    	self.send_event(('','',''))
+    	# remove object from global index
+        self._del_idx_obj_data(self) ;
+        del self ;
+        
+        
