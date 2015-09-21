d9f1479b513fc85f04e7c99557f1880752c6fadf
joaK <kruithof@pequan.lip6.fr>
2015-09-21 17:02:34 +0200

    add spacing (cosmetic)

--- fipogen2/LTI/random_dSS.py
+++ fipogen2/LTI/random_dSS.py
@@ -6,10 +6,15 @@ from numpy.random         import rand, randn
 from numpy.linalg         import solve, LinAlgError
 
 # Random state-space
+
 def random_dSS(n=None, p=1, q=1):
-    """Generate a n-th order random stable state-space, with p inputs and q outputs
+  
+    """
+   
+    Generate a n-th order random stable state-space, with p inputs and q outputs
     
     copy/Adapted from control-python library
+    
     """
     
     if n == None:
@@ -29,6 +34,7 @@ def random_dSS(n=None, p=1, q=1):
     pDzero = 0.2
                 
     # Check for valid input arguments.
+    
     if n < 1 or n % 1:
             raise ValueError(("states must be a positive integer.  #states = %g." % n))
     if p < 1 or p % 1:
@@ -37,81 +43,107 @@ def random_dSS(n=None, p=1, q=1):
         raise ValueError(("outputs must be a positive integer.  #outputs = %g." % q))
                 
     # Make some poles for A.  Preallocate a complex array.
+    
     poles = zeros(n) + zeros(n) * 0.j
     i = 0
                 
     while i < n:
+      
         if rand() < pRepeat and i != 0 and i != n - 1:
+	  
             # Small chance of copying poles, if we're not at the first or last
             # element.
+            
             if poles[i - 1].imag == 0:
-                # Copy previous real pole.
-                poles[i] = poles[i - 1]
+                
+                poles[i] = poles[i - 1] # Copy previous real pole.
                 i += 1
+                
             else:
-                # Copy previous complex conjugate pair of poles.
-                poles[i:i + 2] = poles[i - 2:i]
+                
+                poles[i:i + 2] = poles[i - 2:i] # Copy previous complex conjugate pair of poles.
                 i += 2
+                
         elif rand() < pReal or i == n - 1:
-            # No-oscillation pole.
-            poles[i] = 2. * rand() - 1.
+            
+            poles[i] = 2. * rand() - 1. # No-oscillation pole.
             i += 1
+            
         else:
-            # Complex conjugate pair of oscillating poles.
-            mag = rand()
+            
+            mag = rand() # Complex conjugate pair of oscillating poles.
             phase = 2. * pi * rand()
             poles[i] = complex(mag * cos(phase), mag * sin(phase))
             poles[i + 1] = complex(poles[i].real, -poles[i].imag)
             i += 2
 
     # Now put the poles in A as real blocks on the diagonal.
+    
     A = zeros((n, n))
     i = 0
+    
     while i < n:
+      
         if poles[i].imag == 0:
+	  
             A[i, i] = poles[i].real
             i += 1
+            
         else:
+	  
             A[i, i] = A[i + 1, i + 1] = poles[i].real
             A[i, i + 1] = poles[i].imag
             A[i + 1, i] = -poles[i].imag
             i += 2
-    # Finally, apply a transformation so that A is not block-diagonal.
-    while True:
+    
+    while True: # Finally, apply a transformation so that A is not block-diagonal.
+      
         T = randn(n, n)
+        
         try:
             A = dot(solve(T, A), T)  # A = T \ A * T
             break
+	  
         except LinAlgError:
             # In the unlikely event that T is rank-deficient, iterate again.
             pass
 
     # Make the remaining matrices.
+    
     B = randn(n, p)
     C = randn(q, n)
     D = randn(q, p)
 
     # Make masks to zero out some of the elements.
+    
     while True:
+      
         Bmask = rand(n, p) < pBCmask 
         if not Bmask.all():  # Retry if we get all zeros.
             break
     
     while True:
+      
         Cmask = rand(q, n) < pBCmask
         if not Cmask.all():  # Retry if we get all zeros.
             break
     
     if rand() < pDzero:
+      
         Dmask = zeros((q, p))
+        
     else:
+      
         while True:
+	  
             Dmask = rand(q, p) < pDmask
+            
             if not Dmask.all():  # Retry if we get all zeros.
                 break
     
 
     # Apply masks.
+    
     B = B * Bmask
     C = C * Cmask
     D = D * Dmask
