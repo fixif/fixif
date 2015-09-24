2f8af290695b0bf921911c8d0d85b20b365a4b9b
joaK <kruithof@pequan.lip6.fr>
2015-09-24 10:09:31 +0200

    add example from manual

---
+++ fipogen2/examples/0_manual_tutorial.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#coding=utf8 
+
+"""
+This test script is used as a tool to test FIPOgen functionality
+using a workflow defined in the manual's example
+"""
+# scripts cannot use relative imports
+import sys,os
+sys.path.insert(0, os.path.abspath('./../'))
+
+from numpy import matrix as mat
+from LTI.dSS import dSS
+
+
+
+def main():
+  
+    """
+  
+    We encapsulate the script in the main function to speedup code 
+  
+    (see interesting :
+  
+    http://stackoverflow.com/questions/419163/what-does-if-name-main-do
+    http://stackoverflow.com/questions/11241523/why-does-python-code-run-faster-in-a-function
+  
+    """
+
+    # create matrixes for Discrete State Space
+    A = mat([[1.4590, -0.91037, 0.39565],[1., 0., 0.],[0., 0.5, 0.]])
+    B = mat([[0.5],[0.],[0.]])
+    C = mat([0.28261, 0.13244, 0.15183])
+    D = mat([0.0031689])
+    
+    mydSS = dSS(A,B,C,D)
+    
+    mySIF = mydSS.toSIF()
+
+
+if __name__ == '__main__':
+    main()
