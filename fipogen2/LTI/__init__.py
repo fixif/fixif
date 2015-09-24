5d1455248b80bda487c582264d19d6d1344273b1
joaK <kruithof@pequan.lip6.fr>
2015-09-24 17:54:17 +0200

    new version of __init__ to try to solve dependencies issues. Still needs a ugly path.insert at deepest (namely Structures) level

---
+++ fipogen2/LTI/__init__.py
@@ -0,0 +1,3 @@
+#coding=utf8
+from dSS import dSS
+from random_dSS import random_dSS
--- fipogen2/__init__.py
+++ fipogen2/__init__.py
@@ -1,4 +1,3 @@
-from .LTI import dSS
-from .TRK import FIPObject, FIPObjLabel, FIPObjEvent
-from .SIF import SIF
+
+
 
