6831d5725eb93465f42a4daaf6fd1f9063060a93
joaK <kruithof@pequan.lip6.fr>
2015-09-25 14:19:49 +0200

    skeleton of RhoDFIIt

---
+++ fipogen2/SIF/Structures/RhoDFIIt/RhoDFIIt.py
@@ -0,0 +1,17 @@
+#coding=UTF8
+
+# This class contains routine to convert to FWR
+
+from SIF import SIF
+
+
+class RhoDFIIt(object):
+	
+	def __init__(self):
+		
+		pass
+		
+	def toSIF(self):
+		
+		pass
+		
