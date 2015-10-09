2a54b46ed6b753314eb67d71a711ce23940b2de8
joaK <kruithof@pequan.lip6.fr>
2015-10-09 15:40:48 +0200

    repo reorg

--- fipogen2/SIF/Structures/__init__.py
+++
@@ -1,4 +0,0 @@
-from State_Space.State_Space import State_Space
-#from rhoDFIIt.rhoDFIIt       import rhoDFIIt
-
-#__all__ = ["State_Space", "rhoDFIIt"]
--- fipogen2/SIF/Structures/empty/empty.py
+++
@@ -1,8 +0,0 @@
-from ...SIF import SIF
-
-# dirty but quick fix otherwise doesn't work
-import sys,os
-sys.path.insert(0, os.path.abspath('../../'))
-from LTI import dSS
-
-
