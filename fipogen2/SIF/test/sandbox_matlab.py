fa07f13388626add12fb90c9118161c77c2f8a0a
joaK <kruithof@pequan.lip6.fr>
2015-10-12 17:53:12 +0200

    legacy matlab tests, new version

--- fipogen2/SIF/test/sandbox_matlab.py
+++ fipogen2/SIF/test/sandbox_matlab.py
@@ -14,7 +14,7 @@ import numpy.testing as npt
 from scipy.io import loadmat
 
 from numpy import matrix as mat
-from numpy import array
+from numpy import array, squeeze, reshape
 
 from Structures import *
 
@@ -33,42 +33,48 @@ def mtlb_save(eng, var):
     print(str_save)
     eng.eval(str_save,nargout=0)
 
-
-
 def mtlb_cleanenv(eng):
-    eng.eval('clear all ; close all ;')
+    eng.eval('clear all ; close all ;',nargout=0)
   
-
-def mtlb_compare(mtlb_eng, mtlb_code, varz, local_varz_dict, decim = 10):
-
-    """
-    Compare value with matlab values for a given executed code
-    """
-    mtlb_cleanenv(mtlb_eng)
-    
-    mtlb_eng.eval(mtlb_code)
-    
-    for var in varz:
-        mtlb_save(var)
-    
-    mtlb_load(filename)
-    
-    npt.assert_almost_equal(mtlb_getArray(var), local_varz_dict[var], decimal=decim)
-
 def mtlb_getArray(target_var):
     
     filename = target_var + '.mat'
     
     h = loadmat(filename)
-    return mat(h[target_var])
+    return h[target_var]
 
 def mtlb_pushCmdGetVar(mtlb_eng, mtlb_code, varz, local_dict):
+    
+    print(mtlb_code)
     
     mtlb_eng.eval(mtlb_code, nargout=0)
     
     for var in varz:
         mtlb_save(mtlb_eng, var)
         local_dict[var] = mtlb_getArray(var)
+
+def mtlb_compare(mtlb_eng, mtlb_code, varz, local_varz_dict, decim = 10):
+
+    """
+    Compare value with matlab values for a given executed code
+    """
+    
+    tmp_dict = {}
+    
+    mtlb_cleanenv(mtlb_eng)
+    
+    mtlb_pushCmdGetVar(mtlb_eng, mtlb_code, varz, tmp_dict)
+    
+    for var in varz:
+    	
+    	#print("Shape of tmp_dict[" + var + "]")
+    	#print(str(tmp_dict[var].shape))
+    	#print("Shape of local_varz_dict["+ var +"]")
+    	#print(str(local_varz_dict[var].shape))
+    	
+        npt.assert_almost_equal(tmp_dict[var], local_varz_dict[var], decimal=decim)
+
+
         
     
 
@@ -78,20 +84,30 @@ def mtlb_pushCmdGetVar(mtlb_eng, mtlb_code, varz, local_dict):
 
 #local_vars = {}
 
-eng = matlab.engine.start_matlab()
+#eng = matlab.engine.start_matlab()
 
 #get num and den from matlab as starting point
 
 mtlb_vars = {}
 local_vars = {}
 
+mtlb_cmd_stack = ''
+
 cmd = '[num, den] = butter(4, 0.05) ;'
 varz = ['num','den']
 
+mtlb_cmd_stack += cmd
+mtlb_cmd_stack += '\n'
+
 mtlb_pushCmdGetVar(eng, cmd, varz, mtlb_vars)
 
+
+cmd = 'H = tf(num,den,1);'
 # create TF matlab obj from num and den
-eng.eval('H = tf(num,den,1);', nargout=0)
+eng.eval(cmd, nargout=0)
+
+mtlb_cmd_stack += cmd
+mtlb_cmd_stack += '\n'
 
 #eng.eval('R1, R2, flag = rhoDFIIt2FWR(H, gamma)')
 
@@ -101,18 +117,18 @@ varz = ['Aq','Bq','Cq','Dq']
 
 mtlb_pushCmdGetVar(eng, cmd, varz, mtlb_vars)
 
+mtlb_cmd_stack += cmd
+mtlb_cmd_stack += '\n'
+
 # create local vars
 
-Aq, Bq, Cq, Dq = tf2ss(array(mtlb_vars['num']), array(mtlb_vars['den']))
+local_vars['Aq'], local_vars['Bq'], local_vars['Cq'], local_vars['Dq'] = tf2ss(squeeze(array(mtlb_vars['num'])), squeeze(array(mtlb_vars['den'])))
 
-local_vars['Aq'] = Aq
-local_vars['Bq'] = Bq
-local_vars['Cq'] = Cq
-local_vars['Dq'] = Dq
+local_vars['Dq'] = local_vars['Dq'].reshape(1,1)
 
 # Test comparison routine
 
-mtlb_compare(eng, cmd, varz, local_vars, decimal = 10)
+mtlb_compare(eng, mtlb_cmd_stack, varz, local_vars, decim = 10)
 
 
 #mtlb_pushCmdGetVar(eng, cmd, varz, local_vars)
