72079634b838f61a738cf4376d7ee35ee1c260d3
joaK <kruithof@pequan.lip6.fr>
2015-10-14 18:00:56 +0200

    reflect structures folder change in init

--- fipogen2/Structures/__init__.py
+++ fipogen2/Structures/__init__.py
@@ -3,7 +3,5 @@ from DFII.DFII                           import DFII
 from Modal_Delta.Modal_Delta             import Modal_Delta
 from RhoDFIIt.RhoDFIIt                   import RhoDFIIt
 from State_Space.State_Space             import State_Space
-from Transfer_Function.Transfer_Function import Transfer_Function
-
 
 #__all__ = ["State_Space", "rhoDFIIt"]
