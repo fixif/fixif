a0c13e49057e84889453a792f5de752b5ce2b9c6
joaK <kruithof@pequan.lip6.fr>
2015-10-19 16:53:37 +0200

    lmast version of DFI : problem with matrix sizes

--- fipogen2/Structures/DFI/DFI.py
+++ fipogen2/Structures/DFI/DFI.py
@@ -3,7 +3,8 @@
 from SIF import SIF
 
 from numpy import matrix as mat
-from numpy import diag, zeros, eye, rot90, ones, r_, c_
+from numpy import diagflat, zeros, eye, rot90, ones, r_, c_, atleast_2d
+from numpy.linalg import inv
 
 class DFI(SIF):
   
@@ -52,32 +53,43 @@ class DFI(SIF):
         
         # gamma1 and gamma4 differ between options
         if opt is 1:
-            gamma1 = num[0,1:] - den[0,1:]
+            gamma1 = num[0, 1:] - den[0, 1:]
         elif opt is 2:
             # Here a potential problem arises ?? verify with thibault
-            gamma1 = r_[c_[num[0,1:], zeros((1,nnum))], zeros((1,nnum)) - den[0,1:]]
+            gamma1 = r_[c_[num[0, 1:], zeros((1, nnum))], zeros((1, nnum)) - den[0, 1:]]
     # ???    
         #gamma2 = [[diag(ones((1,nnum-1)),-1), zeros((nnum, nden))],[zeros((nden,nnum)), diag(ones((1,nden-1)),-1)]]
-        gamma2 = r_[c_[diagflat(ones((1,nnum-1)),-1), zeros((nnum, nden))],c_[zeros((nden,nnum)), diagflat(ones((1,nden-1)),-1)]]
+        gamma2 = r_[c_[diagflat(ones((1, nnum-1)), -1), zeros((nnum, nden))],c_[zeros((nden, nnum)), diagflat(ones((1, nden-1)), -1)]]
         #CurrentWork
-        gamma3 = [[1], [zeros((nnum+nden-1, 1))]]
+        gamma3 = r_[atleast_2d(1), zeros((nnum+nden-1, 1))]
     
         if opt is 1:
-            gamma4 = [[zeros((nnum,1))],[1],[zeros((nden-1,1))]]
+            gamma4 = r_[zeros((nnum, 1)), atleast_2d(1), zeros((nden-1, 1))]
         elif opt is 2:
-            gamma4 = [[zeros((nnum,2))],[1,1],[zeros((nden-1,2))]]
+            gamma4 = r_[zeros((nnum, 2)), atleast_2d([1,1]), zeros((nden-1, 2))]
             
         # transformation to 'optimize' the code
         
+        gamma1 = mat(gamma1)
+        gamma2 = mat(gamma2)
+        gamma3 = mat(gamma3)
+        gamma4 = mat(gamma4)
+        
         T = mat(rot90(eye(2*nnum)))
+        
+        invT = inv(T)
         
         # build SIF
         
-
-        JtoS = den[0], gamma4, [1], gamma1, num[0], gamma2, gamma3, mat(zeros((1, nnum+nden))), [0]
+        if opt is 1:
+        	
+            JtoS = den[0,0], invT*gamma4, atleast_2d(1), gamma1*T, atleast_2d(num[0,0]), invT*gamma2*T, invT*gamma3, mat(zeros((1, nnum+nden))), atleast_2d(0)
         
+        elif opt is 2:
+        	
+        	JtoS = mat(eye(2)), invT*gamma4, mat([1,1]), gamma1*T, r_[[num[0,0]],[0]], invT*gamma2*T, invT*gamma3, mat(zeros((1,nnum+nden))), atleast_2d(0)
         # define event
         
-        SIF.__init__(self, JtoS, DFI_father_obj, **DFI_event)
+        SIF.__init__(self, JtoS, eps, DFI_father_obj, **DFI_event)
         
         
