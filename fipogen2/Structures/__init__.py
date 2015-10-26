064c8167fe7d1dac70dad55a383d8d82b8a81c99
joaK <kruithof@pequan.lip6.fr>
2015-10-26 15:08:33 +0100

    add struct to init.py

--- fipogen2/Structures/__init__.py
+++ fipogen2/Structures/__init__.py
@@ -1,6 +1,6 @@
 from DFI.DFI                             import DFI
 from DFII.DFII                           import DFII
-from Modal_Delta.Modal_Delta             import Modal_Delta
+#from Modal_Delta.Modal_Delta             import Modal_Delta
 from RhoDFIIt.RhoDFIIt                   import RhoDFIIt
 from State_Space.State_Space             import State_Space
 
