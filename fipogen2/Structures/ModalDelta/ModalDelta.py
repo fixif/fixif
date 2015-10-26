5d0313935b1e6fcbb64c09243d7c90aface19bd7
joaK <kruithof@pequan.lip6.fr>
2015-10-26 17:48:48 +0100

    implementation in progress

--- fipogen2/Structures/ModalDelta/ModalDelta.py
+++ fipogen2/Structures/ModalDelta/ModalDelta.py
@@ -8,6 +8,9 @@ from numpy import matrix as mat
 from numpy import zeros, diag, real, imag, diagflat
 from numpy.linalg import eig, lstsq
 
+# relaxedl2scaling
+from numpy import ones, log2, floor, sqrt #diagflat
+
 class ModalDelta(SIF):
 
     def canon_modal(A, B, C, D):
@@ -15,6 +18,11 @@ class ModalDelta(SIF):
         """
         Canon modal form of dSS
         """
+        
+        A = mat(A)
+        B = mat(B)
+        C = mat(C)
+        D = mat(D)
         
         # Modal form
         mylambda, E = eig(A)
@@ -32,20 +40,21 @@ class ModalDelta(SIF):
             while k <= (mylambda.shape[0]-1):
           
                 if imag(mylambda[k]) is not 0:
-                	
+                    
                     rel = real(mylambda[k])
                     iml = imag(mylambda[k])
                     T[:,k] = real(V[:,k])
                     T[:,k+1] = imag(V[:,k])
-                    E[k:k+1,k:k+1] = c_[[rel, iml],[-iml, rel]]
+                    E[k:k+1,k:k+1] = c_[r_[rel, iml],r_[-iml, rel]]
                     k = k + 2
                     
                 else:
-                	
+                    
                     T[:,k] = V[:,k]
                     k = k + 1
             
         else:
+            
             T = V
             
         
@@ -56,10 +65,46 @@ class ModalDelta(SIF):
         
         return A, B, C, D
   
-    def relaxedl2scaling(mySIF):
+    def relaxedl2scaling(mySIF, Umax=None, delta=None):
         
+        # change of behaviour vs. old code : if Umax is specified it overrides other data
         
+        if Umax is None:
         
+            if not hasattr(mySIF, 'FPIS'):
+                Umax = 1 # any power of 2
+            else:
+                Umax = mySIF.FPIS.Umax
+        
+        if delta is None:
+            delta = 1
+            
+        # SISO test
+        
+        l, m, n, p = mySIF.size()
+        
+        if not ((m is not 1) and (p is not 1)):
+            raise('The system must be SISO!')
+        
+        # alpha
+        
+        if not hasattr(mySIF, 'FPIS'):
+            alphaX = mat(- ( log2(Umax) - floor(log2(Umax)) )*ones((n, 1)))
+            alphaT = mat(- ( log2(Umax) - floor(log2(Umax)) )*ones((l, 1)))
+        else:
+            alphaX = mat( mySIF.FPIS.betaX - (mySIF.FPIS.betaU + log2(Umax) - floor(log2(Umax)))*ones(n, 1))
+            alphaT = mat( mySIF.FPIS.betaT - (mySIF.FPIS.betaU + log2(Umax) - floor(log2(Umax)))*ones(l, 1))
+            
+        # U, V, W transformation matrixes
+        
+        def toto(self, M, alpha = None, beta = None):
+            
+            if alpha is not None:
+                
+                d = delta*sqrt(diagflat(M))
+                
+                #WORK_MARKER
+                
         
         return SIF, Y ,W
         
@@ -86,8 +131,10 @@ class ModalDelta(SIF):
         if Delta is None:
             Delta = zeros((1, Aq.shape[0]))
             isDeltaExact = True
-        elsif (Delta is not None and isDeltaExact is None):
+        elif (Delta is not None and isDeltaExact is None):
             isDeltaExact = False
+    
+        # Conflit dans le code matlab, voir ModalDelta2FWR.m
     
         #if isDeltaExact is None: # see https://www.python.org/dev/peps/pep-0008/#id42 / point 2
         #isDeltaExact = False
@@ -99,6 +146,6 @@ class ModalDelta(SIF):
         
         R_modale = State_Space(A, B, C, D)
     
-
+        #R_modale = State_Space(canon_modal(mat(A), mat(B), mat(C), mat(D)))
     
     
