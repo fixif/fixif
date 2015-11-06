4c4dd31e34a267ea5731a95a96443ab3de1c9371
joaK <kruithof@pequan.lip6.fr>
2015-11-06 10:30:32 +0100

    create script folder

---
+++ fipogen2/scripts/benoit_11_2015/script_benoit.py
@@ -0,0 +1,15 @@
+#coding=utf8
+#!usr/bin/env python
+
+from sys import path
+path.append('../../')
+
+import LTI
+import Structures
+import SIF
+
+def main() 
+    
+
+if __name__ is "__main__":
+    main()
