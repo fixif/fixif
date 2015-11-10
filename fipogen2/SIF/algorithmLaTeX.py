55625a6a3798c60beb28c2809c28c4dd456b512a
joaK <kruithof@pequan.lip6.fr>
2015-11-10 18:14:43 +0100

    new version algolatex

--- fipogen2/SIF/algorithmLaTeX.py
+++ fipogen2/SIF/algorithmLaTeX.py
@@ -12,10 +12,10 @@ __email__ = "joachim.kruithof@lip6.fr"
 __status__ = "Beta"
 
 import SIF
-from jinja2 import Environment, FileSystemLoader, PackageLoader
+from jinja2 import Environment, PackageLoader
 from numpy import tril, all, eye, zeros, where, abs, matrix, r_, c_
 from numpy import matrix as mat
-
+import algorithmAuxFunc
 
 # Found inspiration here
 # http://martin-ueding.de/en/latex/computation-results/index.html
@@ -33,76 +33,16 @@ def algorithmLaTeX(R, out_file, caption=None):
     """
     
     def _genVarName(baseName, numVar):
-        
-        varNames = []
-        
-        if numVar is 1:
-            varNames.append(baseName)
-        else:
-            for i in range(0,numVar):
-                varNames.append(baseName + "_{" + str(i+1) + "}")
-                
-        return varNames
-    
-    def _scalprodCdouble(P, names):
-        
-        """
-        Return the C-code corresponding to a double floating-point scalar product
-        (the vector of coefficient 'P' by the vector of variables 'name').
-        
-        Ex: P(1)*name(1) + P(2)*name(2) + ... + P(n)*name(n)
-        
-        Syntax:
-         S = scalprodCdouble(  P, name)
-        
-        Parameters:
-        
-        - P: vector of coefficients used in the scalar product (np.matrix, so 2d)
-        - name: name of the variables
-        
-        Returns string
-        
-        WARNING
-        string conversion of float induces a rounding of the number (was the case in matlab)
-        
-        matlab
-        
-        d=0.09789879876298743
-        num2str(d,'%.6g')
-        0.0978988
-        
-        str(d,'%.6g')
-        """
-        
-        tol = 1.e-10
-        
-        dec_format = 6 # 255
-        
-        S = ""
-        
-        (rows,cols) = where(abs(P)>tol)
-        
-        for i in range(0,rows.size):
-        
-            pos = rows[i],cols[i]
-            vecpos = cols[i]
-        
-            if abs(P[pos]-1.)<tol:
-                S += "+ " + names[vecpos] + "\\\n"
-            elif abs(P[pos]+1.)<tol:
-                S += "- " + names[vecpos] + "\\\n"
-            else:
-                S+= "+ " + ("{:."+str(dec_format)+"g}").format(P[pos]) + "*" + names[vecpos] + "\\\n"
-        
-            if i is (rows.size - 1):
-                S += "\n"
-        
-        S = S[1:-3]
-            
-        if S == "":
-            S += "0"
-        
-        return S
+		
+	varNames = []
+		
+	if numVar is 1:
+		varNames.append(baseName)
+	else:
+		for i in range(0,numVar):
+			varNames.append(baseName + "_{" + str(i+1) + "}")
+				
+	return varNames
     
     env = Environment(loader=PackageLoader('SIF','templates'),
       block_start_string= '%<',
