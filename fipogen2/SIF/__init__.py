65f56e40e18c911089eecc024d80da63fe178ccb
joaK <kruithof@pequan.lip6.fr>
2015-11-20 18:27:44 +0100

    less dirty __init__

--- fipogen2/SIF/__init__.py
+++ fipogen2/SIF/__init__.py
@@ -2,9 +2,4 @@
 # SIF package from FiPogen2
 
 from SIF import SIF
-from Structures import State_Space
-from algorithmLaTeX import algorithmLaTeX
-from algorithmCfloat import algorithmCfloat
-from MsensH import MsensH
-
-#__all__ = ["SIF", "Structures"]
+#from Structures import State_Space
