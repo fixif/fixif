250f914935264d56b81d2eecaa857b8433fd9c9c
joaK <kruithof@pequan.lip6.fr>
2015-11-30 17:23:21 +0100

    working version of gen_data ; conversion in one direction (ss->tf) does not work.

--- fipogen2/func_aux/gen_data.py
+++ fipogen2/func_aux/gen_data.py
@@ -15,14 +15,14 @@ __email__ = "joachim.kruithof@lip6.fr"
 __status__ = "Beta"
 
 from scipy.signal import butter, tf2ss, ss2tf
+from numpy import arange
+from LTI import *
+from itertools import chain
 
-import LTI
 
 def gen_data(data_type, data_source, opt):
     
     data_dict = {}
-    
-    is_data_TF = True
     
     ev_source = "func"
     ev_subsource = ""
@@ -33,8 +33,8 @@ def gen_data(data_type, data_source, opt):
         Generate TF or SS depending on scipy function used
         """
       
-        butter_range_order = range(4, 1, 10)
-        butter_range_cut = range(0.05, 0.01, 0.11)
+        butter_range_order = arange(4, 10, 1)
+        butter_range_cut   = arange(0.05, 0.11, 0.01)
       
         list_obj = []
       
@@ -47,59 +47,89 @@ def gen_data(data_type, data_source, opt):
         
                 for cut in butter_range_cut:
           
+                    #print("order = {0} cut = {1}".format(order,cut))
                     num, den = butter(order,cut)
-                    
                     list_obj.append(dTF(num, den, e_source = ev_source, e_subsource = ev_subsource))
         
-        return list_obj
+        return list_obj, is_data_TF
      
     def gen_data_random():
-    	
-    	"""
-    	Generate random state spaces
-    	"""
-    	
-    	is_data_TF = False
-    	ev_subsource = "random_dSS"
-    	
-    	# number of state spaces to generate
-    	num_ss = 50
-    	
-    	# range / order of state_space
-    	random_n_order = range(1,1,20)
-    	#range of inputs
-    	random_p_range = range(1,1,5)
-    	#range of outputs
-    	random_q_range = range(1,1,5)
-    	
-    	for n in random_n_order:
-    		
-    		for p in random_p_range:
-    			
-    			for q in random_q_range:
-    				
-    				list_obj.append(random_dSS(n, p, q))
-    	
+        
+        """
+        Generate random state spaces
+        """
+        list_obj = []
+        is_data_TF = False
+        ev_subsource = "random_dSS"
+        
+        # range / order of state_space
+        random_n_order = range(1,20,1)
+        #range of inputs
+        random_p_range = range(1,5,1)
+        #range of outputs
+        random_q_range = range(1,5,1)
+        
+        for n in random_n_order:
+            for p in random_p_range:
+                for q in random_q_range:
+                    
+                    list_obj.append(random_dSS(n, p, q)) # TODO add tracking to random_dSS or modify to get dSS(*chain(random_ABCD(n,p,q)), e_source=ev_subsource)
+        
+        return list_obj, is_data_TF
         
     if data_source == "signal":
-            
-        list_obj = gen_data_signal(opt)
-            
+        list_obj, is_data_TF = gen_data_signal(opt)
     elif data_source == "random":
+        list_obj, is_data_TF = gen_data_random()
 
-        list_obj = gen_data_random()
-    
-    
     if data_type == "SS" and is_data_TF: # convert to SS from TF for output
-    
-        list_obj = [dSS(tf2ss(obj.num, obj.den), e_source = ev_source, e_subsource=ev_subsource) for obj in list_obj]
+        #http://docs.scipy.org/doc/numpy/reference/generated/numpy.matrix.A1.html
+        #numpy.matrix.A1 <=> np.asarray(x).ravel
+        print('Converting TF to SS')
+        
+        obj_list = []
+        
+        for obj in list_obj:
+            A, B, C, D = tf2ss(obj.num.A1, ob.den.A1)
+            list_obj.append(dSS(A, B, C, D, e_source = ev_source, e_subsource=ev_subsource))
+        
+        list_obj = obj_list
+        
+        #list_obj = [dSS(*chain(tf2ss(obj.num.A1, obj.den.A1)), e_source = ev_source, e_subsource=ev_subsource) for obj in list_obj]
     
     elif data_type == "TF" and not(is_data_TF): # convert to TF from SS for output
       
-        list_obj = [dTF(ss2tf(obj.A, obj.B, obj.C, obj.D), e_source = ev_source, e_subsource=ev_subsource) for obj in list_obj]
+        print("Converting SS to TF NOT IMPLEMENTED")
+        
+        obj_list = []
+        for obj in list_obj:
+            
+            num, den = ss2tf(obj.A, obj.B, obj.C, obj.D)
+            print("********************************************************")
+            print('A')
+            print(obj.A)
+            print('B')
+            print(obj.B)
+            print('C')
+            print(obj.C)
+            print('D')
+            print(obj.D)
+            print('numerator')
+            print(num)
+            print('denominator')
+            print(den)
+            print("********************************************************")
+            obj_list.append(dTF(num, den, e_source=ev_source, e_subsource=ev_subsource))
+        
+        list_obj = obj_list
       
-    else:
-      
+    elif data_type == "SS" and not(is_data_TF):
+        print("data is SS and we need SS : doing nothing")
+    
+    elif data_type == "TF" and is_data_TF:
+        print("data is TF and we need TF : doing nothing")
+
+    else:  
         raise(ValueError, "data_type not known")
     
     return list_obj
