ee34091ad11461b8986f1d24636162a967c2b342
joaK <kruithof@pequan.lip6.fr>
2015-12-18 11:33:24 +0100

    rhodfIIt OK ; changed init because RhoDFIIt needs to be a suybclass of State_Space

--- fipogen2/Structures/RhoDFIIt/RhoDFIIt.py
+++ fipogen2/Structures/RhoDFIIt/RhoDFIIt.py
@@ -4,20 +4,22 @@
 
 from SIF import SIF
 from LTI import TF, dSS
-from numpy import zeros, all, poly, matrix, sqrt, prod, transpose, diagflat, r_, c_
+from Structures import State_Space
+
+from numpy import zeros, all, poly, matrix, sqrt, prod, transpose, diagflat, r_, c_, ones, atleast_2d, eye
 from numpy import matrix as mat
 from numpy.linalg import cond, inv
 
-class RhoDFIIt(SIF):
+
+
+class RhoDFIIt(State_Space):
 
     @staticmethod
     def F2(x):
         return log2(x)-floor(log2(x))
 
-    def __init__(self, num, den, gamma, isGammaExact = True, delta = None, isDeltaExact = False, father_obj = None, cond_limit=1.e20, **event_spec):
-        
-        
-        
+    def __init__(self, num, den, gamma=None, isGammaExact=True, delta=None, isDeltaExact=False, opt=1, cond_limit=1.e20, father_obj = None, **event_spec):
+
         #create default event if no event given
         my_e_type      = event_spec.get('e_type', 'create')
         my_e_subtype   = event_spec.get('e_subtype', 'convert')      
@@ -31,8 +33,11 @@ class RhoDFIIt(SIF):
         RhoDFIIt_father_obj = father_obj
           
         # warning if we don't use matrixes transpose is not going to work correctly 
+              
+        if gamma is None:
+            raise(ValueError, "Specifying gamma is mandatory")
                 
-        if not delta:
+        if delta is None:
             self._delta = mat(zeros(gamma.shape))
         else:
             self._delta = delta
@@ -42,12 +47,12 @@ class RhoDFIIt(SIF):
 
         self._gamma = mat(gamma)
         
-        Va = transpose(mat(self._den))/self._den[0]
-        Vb = transpose(mat(self._num))/self._den[0]
+        Va = transpose(mat(self._den))/self._den[0,0]
+        Vb = transpose(mat(self._num))/self._den[0,0]
         
         # Compare #column (#lines is 1 ?)
-        p1 = Va.shape[1] - 1
-        p2 = Vb.shape[1] - 1
+        p1 = Va.shape[0] - 1
+        p2 = Vb.shape[0] - 1
         p3 = self._gamma.shape[1]
         p4 = self._delta.shape[1]
 
@@ -58,9 +63,8 @@ class RhoDFIIt(SIF):
         
         # =====================================
         # Step 1 : build Valapha_bar, Vbeta_bar
-        # =====================================
-        
         # by considering Delta_k = 1 for all k
+        # =====================================
         
         # Build Tbar
         
@@ -68,28 +72,30 @@ class RhoDFIIt(SIF):
         Tbar[p,p] = 1
         
         for i in range(p-1,-1,-1):
-            Tbar[i, i:p] = poly(self._gamma[0, i:p])# if we use matlab-like syntax, namely i:p+1 there's no check on matrix boundary (if p+1 exceeds boundary we got a result)
+            Tbar[i, i:p+1] = poly(self._gamma[0, i:p].A1)# if we use matlab-like syntax, namely i:p+1 there's no check on matrix boundary (if p+1 exceeds boundary we got a result)
         
         #check for ill-conditioned matrix (relaxed check)
         
         my_cond = cond(Tbar,2)/cond(Tbar,-2) # (larger / smaller) singular value like cond() of MATLAB
         
         if (my_cond > cond_limit):
-            raise('Cannot compute matrix inverse') 
+            raise(ValueError, 'Cannot compute matrix inverse') 
         
         #Valpha_bar, Vbeta_bar
         
+
         Valpha_bar = transpose(inv(Tbar))* Va
         Vbeta_bar  = transpose(inv(Tbar))* Vb
         
         # Equivalent state space (Abar, Bbar, Cbar, Dbar)
         
         A0 = diagflat(mat(ones((p-1,1))),1)
-        A0[0:,0] = - Valpha_bar[1:]
+        
+        A0[:,0] = - Valpha_bar[1:,0]
         
         Abar = diagflat(self._gamma) + A0
         
-        Bbar = Vbeta_bar[1:] - Vbeta_bar[0,0]*Valpha_bar[0, 1:]
+        Bbar = Vbeta_bar[1:,0] - Vbeta_bar[0,0]*Valpha_bar[1:, 0]
         
         Cbar = mat(zeros((1,p)))
         Cbar[0,0] = 1
@@ -105,14 +111,14 @@ class RhoDFIIt(SIF):
         
         Wc = dSS(Abar, Bbar, Cbar, Dbar).Wc
         
-        if (self._delta == mat(zeros(self._gamma.shape))):
+        if (all(self._delta == mat(zeros(self._gamma.shape)))):
             
             self._delta[0,0] = sqrt(Wc[0,0])
             
             for i in range(1,p):
                 self._delta[0,i] = sqrt( Wc[i,i] / Wc[i-1,i-1] )
         
-        elif delta[0,0] < 0:
+        elif self._delta[0,0] < 0:
             
             self._delta[0,0] = sqrt(Wc[0,0]) * 2^-F2(sqrt(Wc[0,0]))
             
@@ -131,23 +137,27 @@ class RhoDFIIt(SIF):
         Tbar[p,p] = 1
         
         for i in range(p-1,-1,-1):
-            Tbar[i, i:p+1] = poly( self._gamma[0, i:p] / prod(self._delta[0, i:p]))
+            Tbar[i, i:p+1] = poly( (self._gamma[0, i:p] / prod(self._delta[0, i:p])).A1 )
+        
+        
             
-        Ka = mat(prod(self._delta[0,:]))
+        Ka = prod(self._delta[0,:])
         
         Valpha = transpose(inv(Ka * Tbar))*Va
         Vbeta  = transpose(inv(Ka * Tbar))*Vb
+        
+# Valpha, Vbeta OK
         
         # equivalent l2-scaled state space
         
         d = mat(zeros((1,p)))
         
-        for i in range(0, p):
-            d[0,i] = inv( prod(self._delta[0, 0:i]) )
+        for i in range(0, p):    
+            d[0,i] = inv( atleast_2d(prod(self._delta[0, 0:i])) ) # maybe faster to use 1 / blabla here
 
-		# ============================
-		# Step 3 : build SIF from state space or JtoS
-		# ============================
+        # ============================
+        # Step 3 : build SIF from state space or JtoS
+        # ============================
 
         if opt == '1':
 
@@ -160,11 +170,11 @@ class RhoDFIIt(SIF):
             C = Cbar*invTsc
             D = Dbar  # can also be computed from Valpha and Vbeta
 
-            State_Space.__init__(A, B, C, D)
+            State_Space.__init__(self, A, B, C, D)
         
-            A0 = diagflat(ones((p-1,1)),1) # NOT USED ???
+            A0 = mat(diagflat(ones((p-1,1)),1)) # NOT USED ???
         
-            A0[:,0] = -Valpha[0, 1:] # NOT USED ???
+            A0[:,0] = -Valpha[1:, 0] # NOT USED ???
         
             if isGammaExact:
                 for i in range(1,p):
@@ -176,11 +186,11 @@ class RhoDFIIt(SIF):
 
         elif opt == '2':
 
-            K = diagflat(ones((1, p-1)), 1)
+            K = mat(diagflat(ones((1, p-1)), 1))
         
-            K[:,1] = -Valpha[0, 1:p+1] # maybe transpose is needed here ?
+            K[:,1] = -Valpha[1:p+1, 0] # maybe transpose is needed here ?
 
-            JtoS = eye(p), K, c_[1, zeros((1,p-1))], diagflat(delta), r_[Vbeta[0, 1], zeros((p-1, 1))], diagflat(gamma), Vbeta[0, 1:p+1], zeros((1, p)), 0
+            JtoS = eye(p), K, c_[atleast_2d(1), zeros((1,p-1))], mat(diagflat(self._delta)), r_[atleast_2d(Vbeta[1, 0]), zeros((p-1, 1))], mat(diagflat(gamma)), Vbeta[1:p+1, 0], zeros((1, p)), 0
         
-            SIF.__init__(JtoS)
+            SIF.__init__(self, JtoS)
         
