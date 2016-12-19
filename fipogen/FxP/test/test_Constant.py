6f1e6a1be1f99f3780c307e79a03daebb6840090
Thibault Hilaire <thibault.hilaire@lip6.fr>
2016-12-20 00:19:54 +0100

    Cleanup code (Constant.py and test_Constant.py)

--- fipogen/FxP/test/test_Constant.py
+++ fipogen/FxP/test/test_Constant.py
@@ -1,60 +1,76 @@
-import unittest
-from FxP.FPF import FPF
-from FxP.Constant import Constant
+# coding=utf8
+
+"""
+This file contains tests for the Constant class and its methods
+"""
 
 
-########################################################################
-class Test_Constants(unittest.TestCase):
-	""""""
-	def test_construct(self):
-		c = Constant( value=127, wl=8, signed=False)
-		self.assertEqual( c.FPF.wml(), (8,6,-1) )
-		self.assertEqual( c.integer, 254)
+__author__ = "Thibault Hilaire, Benoit Lopez"
+__copyright__ = "Copyright 2015, FIPOgen Project, LIP6"
+__credits__ = ["Thibault Hilaire", "Benoit Lopez"]
 
-		c = Constant( value=127, wl=8)
-		self.assertEqual( c.FPF.wml(), (8,7,0) )
-		self.assertEqual( c.integer, 127)
-		
-		c = Constant( value=-127, wl=8, signed=True)
-		self.assertEqual( c.FPF.wml(), (8,7,0) )
-		self.assertEqual( c.integer, -127)
-		
-		c = Constant( value = 0.36567, wl=8)
-		self.assertEqual( c.FPF.wml(), (8,-1,-8))
-		self.assertEqual( c.integer, 94)
-		self.assertEqual( c.approx, 94*2**-8)
-		
-		with self.assertRaises(ValueError):
-			c = Constant( value=-12, wl=12, signed=False)
+__license__ = "CECILL-C"
+__version__ = "0.4"
+__maintainer__ = "Thibault Hilaire"
+__email__ = "thibault.hilaire@lip6.fr"
+__status__ = "Beta"
 
-		c = Constant( value=127.78, wl=8, signed=False)
-		self.assertEqual( c.FPF.wml(), (8,7,0) )
-		self.assertEqual( c.integer, 128)		
-		
-		# particular cases
-		c = Constant( value=127.78, wl=8, signed=False)
-		self.assertEqual( c.FPF.wml(), (8,7,0) )
-		self.assertEqual( c.integer, 128)		
-		c = Constant( value=-128.1, wl=8, signed=True)
-		self.assertEqual( c.FPF.wml(), (8,7,0) )
-		self.assertEqual( c.integer, -128)					
-		c = Constant( value=127.7, wl=8, signed=True)
-		self.assertEqual( c.FPF.wml(), (8,8,1) )
-		self.assertEqual( c.integer, 64)			
-		
-		c = Constant( value = -64.25, wl=8, signed=True)
-		self.assertEqual( c.FPF.wml(), (8,6,-1))
-		self.assertEqual( c.integer, -128)
-		c = Constant( value=-64.3, wl=8, signed=True)
-		self.assertEqual( c.FPF.wml(), (8,6,-1) )
-		self.assertEqual( c.integer, -128)					
-		c = Constant( value=-64.5, wl=8, signed=True)
-		self.assertEqual( c.FPF.wml(), (8,7,0) )
-		self.assertEqual( c.integer, -65)		
-		
-		# construct with a given FPF
-		with self.assertRaises(ValueError):
-			c = Constant(value=258.54, wl=8,fpf=FPF(8,7,0))
-		
-if __name__ == '__main__':
-	unittest.main()
+
+import pytest
+from fipogen.FxP import FPF
+from fipogen.FxP import Constant
+
+
+def test_construct():
+	"""
+	Test the Constant constructor
+	"""
+	c = Constant(value=127, wl=8, signed=False)
+	assert(c.FPF.wml() == (8, 6, -1))
+	assert(c.integer == 254)
+
+	c = Constant(value=127, wl=8)
+	assert(c.FPF.wml() == (8, 7, 0))
+	assert(c.integer == 127)
+	
+	c = Constant(value=-127, wl=8, signed=True)
+	assert(c.FPF.wml() == (8, 7, 0))
+	assert(c.integer == -127)
+	
+	c = Constant(value=0.36567, wl=8)
+	assert(c.FPF.wml() == (8, -1, -8))
+	assert(c.integer == 94)
+	assert(c.approx == 94*2**-8)
+	
+	with pytest.raises(ValueError):
+		c = Constant(value=-12, wl=12, signed=False)
+
+	c = Constant(value=127.78, wl=8, signed=False)
+	assert(c.FPF.wml() == (8, 7, 0))
+	assert(c.integer == 128)
+	
+	# particular cases
+	c = Constant(value=127.78, wl=8, signed=False)
+	assert(c.FPF.wml() == (8, 7, 0))
+	assert(c.integer == 128)
+	c = Constant(value=-128.1, wl=8, signed=True)
+	assert(c.FPF.wml() == (8, 7, 0))
+	assert(c.integer == -128)
+	c = Constant(value=127.7, wl=8, signed=True)
+	assert(c.FPF.wml() == (8, 8, 1))
+	assert(c.integer == 64)
+	
+	c = Constant(value=-64.25, wl=8, signed=True)
+	assert(c.FPF.wml() == (8, 6, -1))
+	assert(c.integer == -128)
+	c = Constant(value=-64.3, wl=8, signed=True)
+	assert(c.FPF.wml() == (8, 6, -1))
+	assert(c.integer == -128)
+	c = Constant(value=-64.5, wl=8, signed=True)
+	assert(c.FPF.wml() == (8, 7, 0))
+	assert(c.integer == -65)
+	
+	# construct with a given FPF
+	with pytest.raises(ValueError):
+		c = Constant(value=258.54, wl=8, fpf=FPF(8, 7, 0))
+
