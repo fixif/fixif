d875f40f3e7bdeb7015933c259b549868b760d89
Thibault Hilaire <thibault.hilaire@lip6.fr>
2018-04-05 00:26:23 +0200

    add travis-ci support

--- setup.py
+++ setup.py
@@ -1,30 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-	name= 'fipogen',
+	name= 'fixif',
 	version= '0.4',
 	author= 'Thibault Hilaire, Joachim Kruithof, Benoit Lopez, Anastasia Volkova',
 	author_email= 'thibault.hilaire@lip6.fr',
 	packages= find_packages(),  #TODO: list manually the packages we want (FxP, LTI, etc.)
 	scripts= [],
 #    url='http://pypi.python.org/pypi/fipogen/',
-	license= 'CeCCIL-B',
+	license= 'GPLv3',
 	description= 'Finite Word-length Realization toolbox.',
-	long_description= open('README.txt').read(),
+	long_description= open('README.rst').read(),
 	install_requires= [
 		"numpy >= 1.10.4",
 		"scipy >= 0.17.0",
-		"slycot >= 0.2.0",
+#		"slycot >= 0.2.0",
 		"pytest >= 2.8.7",
-		"jinja2 >= 2.8",
-#		"sphinx >= 1.3",
-#		"lxml >= 3.6",
-		"matlabengineforpython",
-		"sollya",
-		"cython",
-		"bottle",
-		"jinja2",
-		"docopt",
+#		"jinja2 >= 2.8",
+#		"matlabengineforpython",
+#		"sollya",
+#		"cython",
+#		"bottle",
+#		"jinja2",
+#		"docopt",
 		"mpmath"
 	],
 	include_package_data=True
