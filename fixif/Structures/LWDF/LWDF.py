a86169b24598f87c5bd1d47407e4171803fa6a0c
Thibault Hilaire <thibault.hilaire@lip6.fr>
2018-04-05 23:28:53 +0200

    LWDF cannot be imported when matlab python engine is not installed

--- fixif/Structures/LWDF/LWDF.py
+++ fixif/Structures/LWDF/LWDF.py
@@ -24,46 +24,50 @@ from fixif.Structures.Structure import Structure
 from numpy import matrix as mat
 #from matlab.engine import connect_matlab
 from fixif.func_aux import MatlabHelper
-
-#import matlab
-
-
-
-
-def makeLWDF(filter):
-	"""
-
-	"""
-	# connect to matlab if not already connected
-	try:
-		MH = MatlabHelper()
-		eng = MH.engine
-
-		#we suppose that we start in the root of the git repository, i.e. at yourlocalpath/fipogen
-		p = os.getcwd() + '/construct/fwrtoolbox/FWRToolbox/'
-		eng.cd(p)
-
-		#eng.addpath('construct', 'fwrtoolbox')
-		#eng.eval('R=ButterLWDF2FWR( %d, %f);'%(filter.n, filter.Wn), nargout=0)
-
-
-		#eng.eval('R=TF2LWDF2SIF( %f, %f);' % (filter.dTF.num.tolist(), filter.dTF.den.tolist()), nargout=0)
-		#R = eng.eval('struct(R)')
-
-
-		R = eng.TF2LWDF2SIF(matlab.double(filter.dTF.num.tolist()), matlab.double(filter.dTF.den.tolist()))
-	except:
-		raise ValueError('Could not create the LWDF structure using matlab.\n')
-
-	# build SIF
-	return { "JtoS": ( (mat(R['J']), mat(R['K']), mat(R['L']), mat(R['M']), mat(R['N']), mat(R['P']), mat(R['Q']), mat(R['R']), mat(R['S'])) ) }
+try:
+	import matlab
 
 
 
-def acceptLWDF(filter):
-	"""
-	a LWDF realization can be build only if the filter is SISO and has EVEN order
-	"""
-	return (filter.dSS._n%2 == 1) and (filter.dSS._p == 1) and (filter.dSS._q == 1)
 
-LWDF = Structure( shortName='LWDF', fullName="Lattice Wave Digital Filter", make=makeLWDF, accept=acceptLWDF)
+	def makeLWDF(filter):
+		"""
+
+		"""
+		# connect to matlab if not already connected
+		try:
+			MH = MatlabHelper()
+			eng = MH.engine
+
+			#we suppose that we start in the root of the git repository, i.e. at yourlocalpath/fipogen
+			p = os.getcwd() + '/construct/fwrtoolbox/FWRToolbox/'
+			eng.cd(p)
+
+			#eng.addpath('construct', 'fwrtoolbox')
+			#eng.eval('R=ButterLWDF2FWR( %d, %f);'%(filter.n, filter.Wn), nargout=0)
+
+
+			#eng.eval('R=TF2LWDF2SIF( %f, %f);' % (filter.dTF.num.tolist(), filter.dTF.den.tolist()), nargout=0)
+			#R = eng.eval('struct(R)')
+
+
+			R = eng.TF2LWDF2SIF(matlab.double(filter.dTF.num.tolist()), matlab.double(filter.dTF.den.tolist()))
+		except:
+			raise ValueError('Could not create the LWDF structure using matlab.\n')
+
+		# build SIF
+		return { "JtoS": ( (mat(R['J']), mat(R['K']), mat(R['L']), mat(R['M']), mat(R['N']), mat(R['P']), mat(R['Q']), mat(R['R']), mat(R['S'])) ) }
+
+
+
+	def acceptLWDF(filter):
+		"""
+		a LWDF realization can be build only if the filter is SISO and has EVEN order
+		"""
+		return (filter.dSS._n%2 == 1) and (filter.dSS._p == 1) and (filter.dSS._q == 1)
+
+	LWDF = Structure( shortName='LWDF', fullName="Lattice Wave Digital Filter", make=makeLWDF, accept=acceptLWDF)
+
+except ImportError:
+	#matlab Python engine is not installed
+	pass
--- fixif/Structures/__init__.py
+++ fixif/Structures/__init__.py
@@ -2,11 +2,15 @@ from fixif.Structures import Structure
 from fixif.Structures.Structure import iterAllRealizations, makeARealization, iterAllRealizationsRandomFilter, iterStructuresAndOptions
 from fixif.Structures.DirectForms.DFI import DFI
 from fixif.Structures.DirectForms.DFII import DFII
-from fixif.Structures.LWDF.LWDF import LWDF
 from fixif.Structures.State_Space.State_Space import State_Space
 from fixif.Structures.rhoDFIIt.rhoDFIIt import rhoDFII
 #from fixif.Structures.LGS_LCW.LGS_LCW import LGS, LCW
 
+try:
+	from fixif.Structures.LWDF.LWDF import LWDF
+except ImportError:
+	# cannot import LWDF when matlab python engine is not installed
+	pass
 
 
 
