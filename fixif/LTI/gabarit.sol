2afdc0c2f223061b5666881bc16c659e608e0194
Anastasia Volkova <anastasia.volkova@lip6.fr>
2018-12-06 10:55:07 +0100

    Corrected copyright in Gabarit

--- fixif/LTI/gabarit.sol
+++ fixif/LTI/gabarit.sol
@@ -1,3 +1,59 @@
+/* Copyright 2016-2018 by
+
+  Laboratoire d'Informatique de Paris 6 - Équipe PEQUAN
+  Sorbonne Universités
+  UPMC Univ Paris 06
+  UMR 7606, LIP6
+  4, place Jussieu
+  F-75252 Paris Cedex 05
+  France
+
+  Laboratoire d'Informatique de Paris 6, equipe PEQUAN,
+  UPMC Universite Paris 06 - CNRS - UMR 7606 - LIP6, Paris, France
+
+  Contributors:
+  Christoph Lauter christoph.lauter@lip6.fr
+  Anastasia Volkova anastasia.volkova@lip6.fr
+
+
+
+  This software is a mathematical library whose purpose is to provide
+  functions to compute the Worst-Case Peak Gain measure of Linear
+  Time-Invariant Digital Filters.
+
+  This software is governed by the CeCILL-C license under French law and
+  abiding by the rules of distribution of free software.  You can  use,
+  modify and/ or redistribute the software under the terms of the CeCILL-C
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info".
+
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited
+  liability.
+
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or
+  data to be ensured and,  more generally, to use and operate it in the
+  same conditions as regards security.
+
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL-C license and that you accept its terms.
+
+  This program is distributed WITHOUT ANY WARRANTY; without even the
+  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+
+ */
+
+
 
 dodebug = false;
 
