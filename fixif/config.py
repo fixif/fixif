70eafe528ddf6c5926d169f7719af0249c48408d
Thibault Hilaire <thibault.hilaire@lip6.fr>
2019-01-25 17:26:29 +0100

    add a config.py file to store configuration items (paths, etc.)

---
+++ fixif/config.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+__author__ = "Thibault HILAIRE"
+__copyright__ = "Copyright 2015, FiXiF Project, LIP6"
+__credits__ = ["Thibault Hilaire"]
+
+__license__ = "GPL v3"
+__version__ = "0.4"
+__maintainer__ = "Thibault Hilaire"
+__email__ = "thibault.hilaire@lip6.fr"
+__status__ = "Beta"
+
+
+"""
+Contains some configurations values, such as paths
+"""
+
+import pkg_resources
+
+SIF_TEMPLATES_PATH = pkg_resources.resource_filename('fixif.fixif', 'SIF/templates/')
